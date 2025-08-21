a83ab8cf06b1191b7da37872155e3affeae1e289
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2025-08-21 11:48:09 -0500

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    files: ['src/**/*.{js,mjs,cjs}'],
+    plugins: { js, '@stylistic': stylistic },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: { '@stylistic/indent': ['error', 2]}
+  }
+])
